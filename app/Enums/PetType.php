fffe7584d392a87557a6264b8634acfd8ad15bb6
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-09-21 22:12:55 +0100

    feat: :sparkles: Enum for pet Type and Cast in model

---
+++ app/Enums/PetType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Enums;
+use Filament\Support\Contracts\HasLabel;
+
+enum PetType: string implements HasLabel {
+    case Dog = 'dog';
+    case Cat = 'cat';
+    case Rat = 'rat';
+    case Lizard = 'lizard';
+    case Rabbit = 'rabbit';
+    case Fish = 'fish';
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::Dog => 'Dog',
+            self::Cat => 'Cat',
+            self::Rat => 'Rat',
+            self::Lizard => 'Lizard',
+            self::Rabbit => 'Rabbit',
+            self::Fish => 'Fish',
+        };
+    }
+}
